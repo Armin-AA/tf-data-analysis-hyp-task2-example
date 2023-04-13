63931eb9f919f22f861eedacbb93400ca56d8f1a
Armin-AA <aat6@yandex.ru>
2023-04-13 21:26:57 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,7 +1,9 @@
 
 import pandas as pd
 import numpy as np
-from scipy.stats import anderson_ksamp
+import scipy.stats as sps
+from hyppo.ksample import MMD
+from scipy import stats
 
 
 chat_id = 98268891 # Ваш chat ID, не меняйте название переменной
@@ -10,9 +12,6 @@ def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    statistic, critical_values, pvalue = anderson_ksamp([x, y])
-    alpha = 0.01
-    if statistic > critical_values[2]:
-        return True
-    else:
-        return False    
+      alpha = 0.01
+      res = MMD(compute_kernel="rbf", gamma=1).test(x, y)[1]
+      return res < alpha
